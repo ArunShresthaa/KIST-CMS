96be96b8f021c59a8fc58ad9b46d640a1f1ce6c5
ArunShrestha <arunstha547@gmail.com>
2026-01-28 15:19:04 +0545

    Add favicon to admin panel and welcome page for improved branding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KIST Medical College - Patient Information Management System</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
